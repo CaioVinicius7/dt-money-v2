949616bcd8fb9645872fa0fa82a3edfefd9c2c9c
Caio Vinícius <caio1525pereira@gmail.com>
2022-08-12 10:37:30 -0300

    Estrutura visual - Setup do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+
+import { defaultTheme } from "./styles/theme/default";
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1> DT Money </h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export { App };
