dbedc4c4d0a34409aba979e5e5c929c749d7cd6a
Caio Vinícius <caio1525pereira@gmail.com>
2022-08-22 18:56:58 -0300

    Performance - Aplicando Context Selectors

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContextSelector } from "use-context-selector";
 
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
@@ -13,7 +13,9 @@ import {
 } from "./styles";
 
 function Transactions() {
-  const { transactions } = useContext(TransactionsContext);
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <div>
